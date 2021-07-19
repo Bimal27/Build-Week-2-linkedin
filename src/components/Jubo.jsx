91983a56de7e4ec706eda1c6c53754459e2a6632
attila <attilavigh.contact@gmail.com>
2021-07-19 10:08:39 +0100

    Initial commit

---
+++ src/components/Jubo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+improt fetchData from './fetchData'
+
+const Jubo = (  ) => {
+
+    const { data, isLoading, isError } = fetchData( url, auth)
+
+    return (  );
+}
+ 
+export default Jubo;
