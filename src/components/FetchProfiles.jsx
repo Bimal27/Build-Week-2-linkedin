19327a31d681ff7df35cedd558200cae9eaa8696
Saikrishnagoka1803 <gskr1803@gmail.com>
2021-07-20 02:29:13 +0100

    Fetch and Show more/less done
    Fetch and Show more/less done

--- src/components/FetchProfiles.jsx
+++ src/components/FetchProfiles.jsx
@@ -2,49 +2,75 @@ import { Component } from "react";
 //import { Authorisation } from "./fetchData";
 import { Container,Row,Col,Button} from "react-bootstrap";
 import PeopleUMayKnow from "./PeopleUMayKnow";
+import {MdUnfoldLess} from 'react-icons/md'
 
-class FetchProfiles extends Component  {
+      
+        
+    
 
-    componentDidMount = async () => {
-        try {
-                let response = await fetch(
-                "https://striveschool-api.herokuapp.com/api/profile/",
-                {
-                    method : 'Get',
-                    headers: {
-                    Authorisation:
-                    'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNWU1M2IzNTgxNzAwMTVjMjI2ZmEiLCJpYXQiOjE2MjY2ODgyODYsImV4cCI6MTYyNzg5Nzg4Nn0.jLBxcWF9mRozIiytcPZgROozVtCo_yFvR70ccS4WpOw',
+
+class fetching extends Component {
+        
+    state={
+        data : [],
+        showlessormore : false
+        }
+        componentDidMount = async () =>{
+            try {
+                const response = await fetch(
+                    "https://striveschool-api.herokuapp.com/api/profile/",
+                    {
+                        method : 'Get',
+                        headers: {
+                        Authorization:
+                        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNWU1M2IzNTgxNzAwMTVjMjI2ZmEiLCJpYXQiOjE2MjY2ODgyODYsImV4cCI6MTYyNzg5Nzg4Nn0.jLBxcWF9mRozIiytcPZgROozVtCo_yFvR70ccS4WpOw',
+                        }
                     }
-                }
-          );
-          let profiles = await response.json();
-          console.log(profiles);
-          
+              );
+              
+    const profiles = await response.json();
+        console.log(profiles);
+        if(response.ok){
+        this.setState({data: profiles, showlessormore: true});
+        }
+        else{
+            console.log('failed')
+            }
         } 
         catch (error) 
             {
             console.log(error);
             }
-      };
+        }
+    
+render(){
+    return(
+    <Container className='mt-5'>
+    <Row className='justify-content-end' >
+    <Col style={{border : '1px solid gainsboro',borderRadius:'8px'}} md={4} xl={3} className='shadow px-2 d-none d-md-block'>
+    <h6 className='text-heading-medium mt-2'><strong>People you may know</strong></h6>
+        {this.state.showlessormore ? this.state.data.slice(0,5).map(e => <PeopleUMayKnow dataobj={e}/>)
+            : this.state.data.slice(0,9).map(e => <PeopleUMayKnow dataobj={e}/>)}        
+               <hr style={{border: '1px solid gainsboro',marginBottom:'0'}}/>
 
-
-    render(){
-        return(
-            <Container className='mt-5'>
-                <Row className='justify-content-end' >
-                <Col md={4} xl={3} className='shadow px-2 d-none d-md-block'>
-                <h6 className='text-heading-medium mt-2'>
-                                <strong>People you may know</strong> 
-                            </h6>
-                        <PeopleUMayKnow />
-                        <PeopleUMayKnow />
-                        <PeopleUMayKnow />
-                        <PeopleUMayKnow />
-                        
-                </Col>
-                </Row>
+             <div className='d-flex justify-content-center'> <Button style={{border:'1px white',fontWeight:'800'}} onClick={() => {this.setState({showlessormore: !this.state.showlessormore})}} variant='outline-secondary'>
+        { this.state.showlessormore ? 'Show more'
+                                    : 'Show less'}
+                             </Button>< MdUnfoldLess /></div>
+            </Col>        
+            </Row>
             </Container>
-        )
-    }
-}
-export default FetchProfiles
+              )
+            }
+        }          
+                                                 
+                                        
+                                        
+                                      
+                                    
+                           
+              
+            
+     
+
+export default fetching
--- src/components/PeopleUMayKnow.jsx
+++ src/components/PeopleUMayKnow.jsx
@@ -1,27 +1,19 @@
-import { Component } from "react";
 import {Button} from "react-bootstrap";
-import sampleimg from './sampleimg.jpeg'
 
 const PeopleUMayKnow = ( props ) => {
-        return(
-            
-                        <section className='pt-1'>
-                            
-                            <ul style={{paddingLeft:'0'}}>
-                                <li className='d-flex' style={{listStyle:'none'}}>
-                                <div><a href="#"><img style={{borderRadius:'52px',width:'52px'}} src={sampleimg} height='50' alt="" /></a></div>
-                                <div style={{paddingLeft:'1rem'}}>
-                                    <a href="#"><h6 style={{marginBottom:'0px',fontSize:'1rem'}}>Sai Krishna Reddy Goka</h6></a>
-                                    <p className='text-muted' style={{marginBottom:'5px',fontSize:'0.8rem'}}>Open for Opportunities | stamp 1G | Web Developer</p>
-                                    <div><Button style={{borderRadius:'1.5rem'}} variant="outline-dark">connect</Button></div>
-                                </div>
-                                </li>
-                                
-                                
-                            </ul>
-                        </section>
-                        
-                    
+    return(
+        <section className='pt-1'>
+            <ul style={{paddingLeft:'0'}}>
+            <li className='d-flex' style={{listStyle:'none'}}>
+            <div><a href="#"><img style={{borderRadius:'52px',width:'52px'}} src={props.dataobj.image} height='50' alt="" /></a></div>
+            <div style={{paddingLeft:'1rem'}}>
+                <h6 style={{marginBottom:'0px',fontSize:'1rem'}}>{props.dataobj.name+'  '+props.dataobj.surname}</h6>
+                <p className='text-muted' style={{marginBottom:'5px',fontSize:'0.8rem'}}>{props.dataobj.title}</p>
+                <div><Button style={{borderRadius:'1.5rem'}} variant="outline-dark">connect</Button></div>
+            </div>
+            </li>
+            </ul>
+        </section>
         )
 }
 export default PeopleUMayKnow
