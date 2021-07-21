f00f07a5b08bc215d57c2350d3f6414a499b039d
VardanGalstian <81989128+VardanGalstyan@users.noreply.github.com>
2021-07-21 17:08:26 +0200

    Latest Update

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -6,6 +6,8 @@ import SidebarOptionalMenue from './SidebarOptionalMenue'
 import Interests from './Interests/Interests.jsx'
 import ViewedProfile from './ProfilesListItems/ViewedProfile'
 import ExperienceList from './Experience/ExperienceList'
+import Dashboard from './DashBoardAndAbout/DashBoard'
+import About from './DashBoardAndAbout/About'
 
 export default function ProfilePage() {
     return (
@@ -13,9 +15,12 @@ export default function ProfilePage() {
             <Row>
                 <Col className='col-md-8 ordder-md-1 pr-2'>
                     <Header/>
+                    <Dashboard/>
+                    <About/>
                     <Activity/>
                     <ExperienceList/>
                     <Interests/>
+                    
                 </Col>
                 <Col className='col-md-4 ordder-md-2 pl-3'>
                     <SidebarOptionalMenue/>
