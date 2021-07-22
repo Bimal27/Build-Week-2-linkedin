3001a02617f826a2ecfd20eb0760bcb3f885b91e
Saikrishnagoka1803 <gskr1803@gmail.com>
2021-07-22 11:27:17 +0100

    Day3
    Day3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 
 import './App.css';
 //import PeopleUMayKnow from './components/PeopleUMayKnow';
-import FetchProfiles from './components/FetchProfiles'
-import EditandDelete from './components/Education_Edit';
+//import FetchProfiles from './components/FetchProfiles'
+//import EditandDelete from './components/Education_Edit';
+import HomeTop from './components/Home-Top';
+import HomeTopModel from './components/Home-Top-Model';
 
 function App() {
   return (
    <>
-   <FetchProfiles/>
-    <EditandDelete/>
+   {/*<FetchProfiles/>
+    <EditandDelete/>*/}
+    <HomeTop/>
+    
     </>
   );
 }
